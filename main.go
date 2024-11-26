4712eb29c92cbd0767c62aa6d3b685b5bbe624cc
DiegoDev <DiegoDev2@users.noreply.github.com>
2024-11-25 20:38:46 -0400

    add actions

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/DiegoDev2/resq/cli"
 )
 
 func main() {
-	// init()
-	cli.Command().Execute()
+	if err := cli.Command().Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
